948b062abaec52212907fdf644ba6c805e50c2c4
jytoumit@gmail.com <jytoumit@gmail.com@ce5c2930-0838-11df-beaf-bb271b2086c6>
2010-01-27 13:19:02 +0000

    introducing preferences

---
+++ trunk/kingdom/src/jyt/game/muxxu/kingdom/puzzle/help/PreferencePanel.java
@@ -0,0 +1,18 @@
+package jyt.game.muxxu.kingdom.puzzle.help;
+
+import java.awt.GridBagLayout;
+
+import javax.swing.JPanel;
+
+public class PreferencePanel extends JPanel
+{
+	public PreferencePanel()
+	{
+		super();
+		setLayout(new GridBagLayout());
+		for (int i = 0; i < Element.values().length; i++)
+		{
+			
+		}
+	}
+}
